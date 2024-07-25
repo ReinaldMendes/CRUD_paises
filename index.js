65e30dfff011d6be982bd2e231c34a7bf9e4a8b5
Reinald Mendes <112445979+ReinaldMendes@users.noreply.github.com>
2024-07-25 09:24:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
 const prompt = require("prompt-sync")();
-const { criar, atualizar, remover, listagem } = require("./crudCidades.js");
+const { criar: criarCidade, atualizar: atualizarCidade, remover: removerCidade, listagem: listarCidades } = require("./crudCidades.js");
+const { criar: criarEstado, atualizar: atualizarEstado, remover: removerEstado, listagem: listarEstados } = require("./crudEstados.js");
+const { criar: criarPais, atualizar: atualizarPais, remover: removerPais, listagem: listarPaises } = require("./crudPaises.js");
+
 while (true) {
-  console.log(` 1 - Criar
-    2- Listar
-    3- Atualizar
-    4 -Remover
-    5- Sair`);
-  const opcao = prompt("Qual opão deseja? : ");
+  console.log(`
+    1 - Criar Cidade
+    2 - Listar Cidades
+    3 - Atualizar Cidade
+    4 - Remover Cidade
+    5 - Criar Estado
+    6 - Listar Estados
+    7 - Atualizar Estado
+    8 - Remover Estado
+    9 - Criar País
+    10 - Listar Países
+    11 - Atualizar País
+    12 - Remover País
+    13 - Sair
+  `);
+
+  const opcao = prompt("Qual opção deseja? : ");
 
   switch (opcao) {
     case "1":
-      criar();
+      criarCidade();
       break;
     case "2":
-      listagem();
+      listarCidades();
       break;
     case "3":
-      atualizar();
+      atualizarCidade();
       break;
     case "4":
-      remover();
+      removerCidade();
       break;
     case "5":
+      criarEstado();
+      break;
+    case "6":
+      listarEstados();
+      break;
+    case "7":
+      atualizarEstado();
+      break;
+    case "8":
+      removerEstado();
+      break;
+    case "9":
+      criarPais();
+      break;
+    case "10":
+      listarPaises();
+      break;
+    case "11":
+      atualizarPais();
+      break;
+    case "12":
+      removerPais();
+      break;
+    case "13":
       process.exit();
-
     default:
+      console.log("Opção inválida, tente novamente.");
       break;
   }
 }
